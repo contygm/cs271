695891b4f2adeb234fc188749f7e91e4ff5ad0c1
contygm <contygm@gmail.com>
2019-07-29 17:59:05 -0700

    p3: all prompts print properly, program compiles

--- program3.asm
+++ program3.asm
@@ -9,40 +9,62 @@ TITLE Composite Number Spreadsheet     (program3.asm)
 
 INCLUDE Irvine32.inc
 
+; --------------------------------------------
 ; constant definitions for minimum and maximum
-MAX		DWORD		300
-MIN		DWORD		1
+; --------------------------------------------
+MAX	= 300
+MIN	= 1
 
 .data
 
 ; intro and exit prompts
-welcomeMsg		"Welcome to the Composite Number Spreadsheet",0
-programTitle	"Programmed by Genevieve Conty",0
-extraCreditMsg	"**EC: Program allows user to choose to display only odd composites.",0
-description0	"This program is capable of generating a list of composite numbers.",0
-description1	"Simply let me know how many you would like to see.",0
-directions		"I’ll accept orders for up to 300 composites.",0
-inputPrompt		"How many composites do you want to view? [1 .. 300]: ",0
-thanksMsg		"Thanks for using my program!",0
+welcomeMsg		BYTE	"Welcome to the Composite Number Spreadsheet",0
+programTitle	BYTE	"Programmed by Genevieve Conty",0
+extraCreditMsg	BYTE	"**EC: Program allows user to choose to display only odd composites.",0
+description0	BYTE	"This program is capable of generating a list of composite numbers.",0
+description1	BYTE	"Simply let me know how many you would like to see.",0
+directions		BYTE	"I will accept orders for up to 300 composites.",0
+inputPrompt		BYTE	"How many composites do you want to view? [1 .. 300]: ",0
+thanksMsg		BYTE	"Thanks for using my program!",0
 
 ; validation error 
-invalidMsg		"Out of range. Please try again.",0
-
+invalidMsg		BYTE	"Out of range. Please try again.",0
 
 .code
 main PROC
 
-; TODO
 ; --------------------------------------------
-; TODO
+; Display instructions and greeting
 ; --------------------------------------------
 introduction:
+	mov		edx, OFFSET welcomeMsg
+	call	WriteString
+	call	CrLf
+
+	mov		edx, OFFSET programTitle
+	call	WriteString
+	call	CrLf
+
+	mov		edx, OFFSET description0
+	call	WriteString
+	call	CrLf
+
+	mov		edx, OFFSET description1
+	call	WriteString
+	call	CrLf
+
+	mov		edx, OFFSET directions
+	call	WriteString
+	call	CrLf
 
 ; TODO
 ; --------------------------------------------
 ; TODO
 ; --------------------------------------------
 getData:
+	mov		edx, OFFSET inputPrompt
+	call	WriteString
+	call	CrLf
 
 ; TODO
 ; --------------------------------------------
@@ -50,23 +72,39 @@ getData:
 ; --------------------------------------------
 validate:
 
+; --------------------------------------------
+; Display error message for failed validation
+; --------------------------------------------
+displayErrorMsg:
+	mov		edx, OFFSET invalidMsg
+	call	WriteString
+	call	CrLf
+;	jmp		getData
+
 ; TODO
 ; --------------------------------------------
-; TODO
+; Display composites based on user-given range
 ; --------------------------------------------
 showComposites:
 
 ; TODO
 ; --------------------------------------------
-; TODO
+; Display only odd composite values
 ; --------------------------------------------
-isComposite:
+oddOnly:
 
 ; TODO
 ; --------------------------------------------
-; TODO
+; Verify whether next number is composite
+; --------------------------------------------
+isComposite:
+
+; --------------------------------------------
+; Display closing message and exit program
 ; --------------------------------------------
 goodbye:
+	mov		edx, OFFSET thanksMsg
+	call	WriteString
 
 	exit	; exit to operating system
 main ENDP
