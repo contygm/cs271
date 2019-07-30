44d26b60339675ef3d66ce0c13f45aabc93559f3
contygm <contygm@gmail.com>
2019-07-29 20:42:20 -0700

    p3: prints correct amount of nums per line, just is composite and EC left

--- program3.asm
+++ program3.asm
@@ -10,14 +10,15 @@ TITLE Composite Number Spreadsheet     (program3.asm)
 INCLUDE Irvine32.inc
 
 ; --------------------------------------------
-; constant definitions for minimum and maximum
+; constant definitions
 ; --------------------------------------------
 MAX	= 300
 MIN	= 1
+MAX_PER_LINE = 9
 
 .data
 
-; intro and exit prompts
+; prompts
 welcomeMsg		BYTE	"Welcome to the Composite Number Spreadsheet",0
 programTitle	BYTE	"Programmed by Genevieve Conty",0
 extraCreditMsg	BYTE	"**EC: Program allows user to choose to display only odd composites.",0
@@ -26,17 +27,29 @@ description1	BYTE	"Simply let me know how many you would like to see.",0
 directions		BYTE	"I will accept orders for up to 300 composites.",0
 inputPrompt		BYTE	"How many composites do you want to view? [1 .. 300]: ",0
 thanksMsg		BYTE	"Thanks for using my program!",0
-
-; validation error 
 invalidMsg		BYTE	"Out of range. Please try again.",0
+spaces			BYTE	"   ",0
+
+; variables
+range			DWORD	?
+lineIndex		DWORD	0
+currentNum		DWORD	1
+isComp			DWORD	0
 
 .code
+
 main PROC
+	call introduction
+	call getData
+	call showComposites
+	call goodbye
+	exit	; exit to operating system
+main ENDP
 
 ; --------------------------------------------
 ; Display instructions and greeting
 ; --------------------------------------------
-introduction:
+introduction PROC
 	mov		edx, OFFSET welcomeMsg
 	call	WriteString
 	call	CrLf
@@ -56,21 +69,32 @@ introduction:
 	mov		edx, OFFSET directions
 	call	WriteString
 	call	CrLf
+	
+	ret
+introduction ENDP
 
-; TODO
 ; --------------------------------------------
-; TODO
+; Prompt user for range of composite numbers
+; and store that number in a variable
 ; --------------------------------------------
-getData:
+getData		PROC
 	mov		edx, OFFSET inputPrompt
 	call	WriteString
-	call	CrLf
+	call	ReadInt
+	mov		range, eax
+	call	validate		; validate that range is between min/max
+	ret
+getData		ENDP
 
-; TODO
 ; --------------------------------------------
-; TODO
+; Validate user input against min/max
 ; --------------------------------------------
-validate:
+validate	PROC
+	cmp		range, MAX		; if input > max
+	jg		displayErrorMsg		
+	cmp		range, MIN		; if input < min
+	jl		displayErrorMsg
+	jmp		valid
 
 ; --------------------------------------------
 ; Display error message for failed validation
@@ -79,36 +103,74 @@ displayErrorMsg:
 	mov		edx, OFFSET invalidMsg
 	call	WriteString
 	call	CrLf
-;	jmp		getData
+	jmp		getData
 
-; TODO
+valid:
+	ret
+
+validate	ENDP
+
 ; --------------------------------------------
 ; Display composites based on user-given range
 ; --------------------------------------------
-showComposites:
+showComposites	PROC
+	mov		ecx, range			; initialize loop counter
+	mov		eax, currentNum
+	call	CrLf
 
-; TODO
+displayNums:
+	call	isComposite
+	cmp		isComp, 1
+	je		print
+	inc		currentNum
+	loop	displayNums
+	jmp		stop	
+
+; TODO ** add choice variable
 ; --------------------------------------------
-; Display only odd composite values
+; Display only odd composite valuess
 ; --------------------------------------------
-oddOnly:
+;oddOnly: 
+
+print:	
+	mov		eax, currentNum
+	call	WriteDec
+	mov		edx, OFFSET spaces
+	call	WriteString
+	inc		currentNum
+	inc		lineIndex
+	cmp		lineIndex, MAX_PER_LINE
+	jg		newLine
+	loop	displayNums
+	jmp		stop
+
+newLine:
+	call	CrLf
+	mov		lineIndex, 0
+	loop	displayNums
+
+stop:
+	call	CrLf
+	ret
+
+showComposites	ENDP
 
 ; TODO
 ; --------------------------------------------
 ; Verify whether next number is composite
 ; --------------------------------------------
-isComposite:
+isComposite		PROC
+	mov		isComp, 1
+	ret
+isComposite		ENDP
 
 ; --------------------------------------------
 ; Display closing message and exit program
 ; --------------------------------------------
-goodbye:
+goodbye		PROC
 	mov		edx, OFFSET thanksMsg
 	call	WriteString
-
-	exit	; exit to operating system
-main ENDP
-
-; (insert additional procedures here)
+	ret
+goodbye		ENDP
 
 END main
